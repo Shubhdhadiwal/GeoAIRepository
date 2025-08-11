53b252b5ebc6e33dbd242f71a508f97d9cbaa4fb
Shubhdhadiwal <dhadiwalshubh348@gmail.com>
2025-08-11 17:38:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ import streamlit.components.v1 as components
 import json
 import os
 from datetime import date
-import geopandas as gpd
 
 # ===== PAGE CONFIG =====
 st.set_page_config(page_title="GeoAI Repository", layout="wide")
